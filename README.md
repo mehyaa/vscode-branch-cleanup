f3bc73c2e4d05436b66657fa59c5e229ff4577ae
mehyaa <mehyaa@gmail.com>
2023-02-08 15:51:35 +0300

    Project files added

---
+++ README.md
@@ -0,0 +1,41 @@
+# Branch Cleanup
+
+A cleaner for VSCode workspace git feature branches.
+
+## Why you need this extension?
+
+VSCode tracks each workspace (opened folder or workspace file) in its workspace storage (a folder in `Code/User/workspaceStorage/<32-length ID>`) which has meta that points to the workspace. When you delete/rename/move workspace, the workspace storage will not update itself and from now on it becomes an unused meta. This extension lists and gives option to delete the unused workspace storage folders.
+
+## Installation
+
+Install from [Marketplace][marketplace-url] page.
+
+## Build
+
+```sh
+$ npm install
+
+$ npm run compile
+```
+
+## Contributing
+
+1. Fork it (<https://github.com/mehyaa/vscode-branch-cleanup>)
+2. Create your feature branch (`git checkout -b feature/<feature_name>`)
+3. Commit your changes (`git commit -am '<type>(<scope>): added some feature'`)
+4. Push to the branch (`git push origin feature/<feature_name>`)
+5. Create a Pull Request
+
+## License
+
+[MIT][license-url]
+
+## Thanks
+
+Icon made by [edt.im][icon-author-url] from [flaticon.com][icon-url]
+
+
+[license-url]: LICENSE
+[marketplace-url]: https://marketplace.visualstudio.com/items?itemName=mehyaa.branch-cleanup
+[icon-url]: https://www.flaticon.com/free-icon/delete_6577290
+[icon-author-url]: https://www.flaticon.com/authors/edtim
---
+++ src/extension.ts
@@ -0,0 +1,103 @@
+import { spawn } from 'child_process';
+
+import { commands, window, workspace, ExtensionContext, QuickPickItem } from 'vscode';
+
+const defaultBranches = ['master', 'main'];
+
+export function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('branch-cleanup.run', async () => {
+    const workspaceFolders = workspace.workspaceFolders;
+
+    if (!workspaceFolders || workspaceFolders.length === 0) {
+      window.showErrorMessage('No workspace folder found');
+
+      return;
+    }
+
+    const branchesQuickPickItems: QuickPickItem[] = [];
+
+    for (const workspaceFolder of workspaceFolders) {
+      const branchNames = await getBranchNames(workspaceFolder.uri.fsPath);
+
+      const workspaceBranchesQuickPickItems = branchNames.map(
+        branchName =>
+          ({
+            label: branchName,
+            description: defaultBranches.includes(branchName) ? 'Default branch' : '',
+            detail: workspaceFolder.uri.fsPath,
+            picked: !defaultBranches.includes(branchName)
+          } as QuickPickItem)
+      );
+
+      branchesQuickPickItems.push(...workspaceBranchesQuickPickItems);
+    }
+
+    const selectedBranchesQuickPickItems = await window.showQuickPick(branchesQuickPickItems, {
+      canPickMany: true,
+      placeHolder: 'Select branches to delete'
+    });
+
+    if (!selectedBranchesQuickPickItems || selectedBranchesQuickPickItems.length === 0) {
+      window.showInformationMessage('No branches selected');
+
+      return;
+    }
+
+    for (const branchesQuickPickItem of selectedBranchesQuickPickItems) {
+      if (branchesQuickPickItem.label && branchesQuickPickItem.detail) {
+        try {
+          await deleteBranch(branchesQuickPickItem.label, branchesQuickPickItem.detail);
+        } catch (err) {
+          window.showErrorMessage(
+            `Failed to delete branch ${branchesQuickPickItem.label} on ${branchesQuickPickItem.detail}.\nError: ${err}`
+          );
+        }
+      }
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+function gitCommand(cmd: string, cwd: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const args = cmd
+      .split(' ')
+      .filter(item => item !== '')
+      .map(item => item.trim());
+
+    const child = spawn('git', args, { cwd });
+
+    let result = '';
+
+    child.stdout.on('data', data => (result += data.toString()));
+    child.stdout.on('error', data => (result += data.toString()));
+    child.stderr.on('data', data => (result += data.toString()));
+    child.stderr.on('error', data => (result += data.toString()));
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    child.on('close', (code: number, signal: string) => {
+      if (code === 0) {
+        resolve(result);
+      } else {
+        reject(new Error(result));
+      }
+    });
+  });
+}
+
+function getBranchNames(cwd: string): Promise<string[]> {
+  return gitCommand('branch', cwd).then(result => {
+    const branchNames = result
+      .split('\n')
+      .map(item => (item.indexOf('*') === 0 ? item.substring(2).trim() : item.trim()));
+
+    return branchNames;
+  });
+}
+
+function deleteBranch(branchName: string, cwd: string): Promise<string> {
+  return gitCommand(`branch -D ${branchName}`, cwd);
+}
